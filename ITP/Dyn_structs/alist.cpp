a20dcbcc8279f662c884b6294df0039e7fff1bef
artema16-01@mail.ru <artema16-01@mail.ru>
2024-04-07 23:10:30 +0400

    ITP. moved struct alist from .cpp to .h to fix incomplete type

--- ITP/Dyn_structs/alist.cpp
+++ ITP/Dyn_structs/alist.cpp
@@ -3,11 +3,7 @@
 
 using namespace std;
 
- struct alist {
-    int item;
-    alist* next;
-    alist* prev;
-};
+
 
  void pushback(alist *&h,alist *&t, int x){
     alist *r = new alist;
@@ -25,6 +21,41 @@ using namespace std;
     t = r;
 
 }
+void insert_after(alist *&h, alist *&t, alist *r, int x){
+    alist *p = new alist;
+    p -> item = x;
+
+    if (r == t){
+        p -> next = NULL;
+        p -> prev = t;
+        t -> next = p;
+        t = p;
+    } else {
+        p -> next = r -> next;
+        p -> prev = r;
+        r -> next -> prev = p;
+
+        r -> next = p;
+
+    }
+
+}
+
+ void input_list(alist *&h, alist *&t){
+
+    int n,x;
+
+    cout << "Введите количество элементов списка n: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++){
+        cin >> x;
+        pushback(h,t,x);
+    }
+
+ }
+
+
  void print(alist *&h,alist *&t){
     alist *r = h;
     while (r) {
--- ITP/Dyn_structs/alist.h
+++ ITP/Dyn_structs/alist.h
@@ -1,8 +1,16 @@
 #ifndef ALIST_H
 #define ALIST_H
- struct alist;
+ struct alist{
+    int item;
+    alist* next;
+    alist* prev;
+};
 
  void pushback(alist *&h,alist *&t, int x);
+
+ void insert_after(alist *&h, alist *&t, alist *r, int x);
+
+ void input_list(alist *&h, alist *&t);
 
  void print(alist *&h,alist *&t);
 
