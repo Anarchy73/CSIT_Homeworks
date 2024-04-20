1b2a9a10ed58ceb3bfb18dbcf4997d5ca32a0bb1
Artyom Solovyev <artema16-01@mail.ru>
2024-04-21 00:04:13 +0400

    ITP. Trees. finished task 1

--- ITP/trees/Trees_tasks/main.cpp
+++ ITP/trees/Trees_tasks/main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 #include "tree.h"
+#include <windows.h>
 using namespace std;
+
+int inordert1(tree *t,int counter){
+    if (t){
+        counter = inordert1(t -> left, counter);
+        if (t -> left && t -> right == NULL) {
+
+                counter++;}
+        counter = inordert1(t -> right, counter);
+    }
+    return counter;
+
+}
+
+void task1(){
+    tree *t = NULL;
+
+    tree_input(t); // 5 3 7 1 9 4 2 8 6 0
+
+
+
+    cout << inordert1(t,0);
+
+}
 
 int main()
 {
-
-    cout << "Hello world!" << endl;
+    SetConsoleOutputCP( 65001 );
+    task1();
     return 0;
 }
