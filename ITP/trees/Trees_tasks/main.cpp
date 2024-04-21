04c64e861d9062eb80be72e2dba2d774cb0b7345
Artyom Solovyev <artema16-01@mail.ru>
2024-04-21 12:30:15 +0400

    ITP. Trees. Added task 2

--- ITP/trees/Trees_tasks/main.cpp
+++ ITP/trees/Trees_tasks/main.cpp
@@ -14,6 +14,14 @@ int inordert1(tree *t,int counter){
     return counter;
 
 }
+int trees_height(tree *&t, int counter){
+    if (t){
+        counter++;
+        counter = max(trees_height(t -> right, counter), trees_height(t -> left,counter));
+
+    }
+    return counter;
+}
 
 void task1(){
     tree *t = NULL;
@@ -22,13 +30,21 @@ void task1(){
 
 
 
-    cout << inordert1(t,0);
+    cout << inordert1(t,0) << endl;
+    inorder(t);
+}
+void task2(){
+    tree *t = NULL;
 
+    tree_input(t); // 5 3 7 1 9 4 2 8 6 0
+
+    cout << "Высота дерева: " << trees_height(t,-1) << endl;
+    inorder(t);
 }
 
 int main()
 {
     SetConsoleOutputCP( 65001 );
-    task1();
+    task2();
     return 0;
 }
